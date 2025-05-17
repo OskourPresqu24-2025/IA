6cd6087b30cc6dad18b85d7fcd4d21b952631a7f
Tristan Marcoux <tristan.marcoux21@gmail.com>
2025-05-17 15:25:31 +0200

    Ajout de la classe Server et intégration dans Program.cs
    - Création de la classe `Server` dans **Server.cs** sous le namespace `IA`.
    - Implémentation des méthodes pour gérer la connexion TCP, l'échange de messages, et la fermeture de la connexion.
    - Ajout d'une méthode `ConnexionPartie` avec validations pour gérer la logique de connexion à une partie.
    - Intégration de la classe `Server` dans **Program.cs** avec appel à `ConnexionPartie()`.
    - Ajout des directives `using` nécessaires dans **Program.cs**.
    - Préparation d'une méthode `AttenteDebutTour` pour une future implémentation.

--- IA/IA/Program.cs
+++ IA/IA/Program.cs
@@ -1,6 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using IA;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
 Console.WriteLine("Hello, World!");
+
+
+Server server = new Server();
+Console.WriteLine (server.ConnexionPartie()) ;
---
+++ IA/IA/Server.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IA
+{
+    public class Server
+    {
+        /// <summary>Le client TCP</summary>
+        private TcpClient client;
+        /// <summary>Le flux entrant depuis le serveur</summary>
+        private StreamReader fluxEntrant;
+        /// <summary>Le flux sortant vers le serveur</summary>
+        private StreamWriter fluxSortant;
+
+        /// <summary>Création du client TCP </summary>
+        private void Connexion()
+        {
+            this.client = new TcpClient("127.0.0.1", 1234);
+        }
+
+        /// <summary>Création du flux entrant et du flux sortant</summary>
+        private void CreationFlux()
+        {
+            this.fluxEntrant = new StreamReader(this.client.GetStream());
+            this.fluxSortant = new StreamWriter(this.client.GetStream())
+            {
+                AutoFlush = true
+            };
+        }
+
+        /// <summary>Etablir la connexion avec le serveur</summary>
+        public void EtablirConnexion()
+        {
+            this.Connexion();
+            this.CreationFlux();
+        }
+
+        /// <summary>Envoyer un message au serveur</summary>
+        /// <param name="message">Le message à envoyer</param>
+        public void EnvoyerMessage(string message)
+        {
+            Console.WriteLine(">>" + message);
+            this.fluxSortant.WriteLine(message);
+        }
+
+        /// <summary>Recevoir un message depuis le serveur (bloque jusqu'à réception d'un message)</summary>
+        public string RecevoirMessage()
+        {
+            string message = this.fluxEntrant.ReadLine();
+            Console.WriteLine("<< " + message);
+            return message;
+        }
+
+        /// <summary>Termine la connexion au serveur</summary>
+        public void FermerConnexion()
+        {
+            this.client.Close();
+        }
+
+        public int ConnexionPartie()
+        {
+            this.EtablirConnexion();
+            var messageServer = this.RecevoirMessage();
+            if (messageServer != "NOM_EQUIPE")
+            {
+                throw new InvalidOperationException("Erreur nomEquipe "+messageServer);
+            }
+            this.EnvoyerMessage("Oskour");
+            messageServer = this.RecevoirMessage();
+            if ( !messageServer.StartsWith("Bonjour"))
+            {
+                throw new InvalidOperationException("Erreur bienvenue"+ messageServer);
+            }
+
+            return int.Parse(messageServer.Split("|")[1]); 
+
+        }
+
+        public void AttenteDebutTour()
+        {
+                
+        }
+    }
+}
